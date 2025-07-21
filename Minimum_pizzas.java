37f0ee4cde0827f74bfae9284740ae47bbd96fc1
Praveena Venkatraj <praveena655.cse@gmail.com>
2025-07-21 21:24:26 +0530

    Create Minimum_pizzas.java

---
+++ Minimum_pizzas.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++){
+		    int members=sc.nextInt();
+		    int slice=sc.nextInt();
+		    int n=((slice*members)+3)/4;
+		    System.out.println(n);
+		     // int totalSlices = members * slice;
+		    // int pizzas = (int)Math.ceil((double)totalSlices / 4);
+		   //System.out.println(pizzas);
+		    
+		}
+	}
+}
