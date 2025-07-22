973cf6309513cc80c31a83f0dc88b47c680c26e4
Praveena Venkatraj <praveena655.cse@gmail.com>
2025-07-22 21:38:07 +0530

    Create Count_the_notebooks.java

---
+++ Count_the_notebooks.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+		    int n=sc.nextInt();
+		    int m=n*1000;
+		    int o=m/100;
+		    System.out.println(o);
+		}
+
+	}
+}
