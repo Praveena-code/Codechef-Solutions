184c2913494bfb994c0ddc7b9e2bc7c7904f1c51
Praveena Venkatraj <praveena655.cse@gmail.com>
2025-09-13 22:20:45 +0530

    Create Get_lowest_free.java

---
+++ Get_lowest_free.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+		    int a = sc.nextInt();
+		    int b = sc.nextInt();
+		    int c = sc.nextInt();
+		    if(a<=b && a<=c){
+		        System.out.println(b+c);
+		    }
+		    else if(b<=a && b<=c){
+		        System.out.println(a+c);
+		    }
+		    else{
+		        System.out.println(b+a);
+		    }
+		}
+
+	}
+}
