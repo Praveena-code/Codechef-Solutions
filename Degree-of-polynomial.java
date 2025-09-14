5247ccd61df5fb74d6e7ad4d123b5f12eeedee2d
Praveena Venkatraj <praveena655.cse@gmail.com>
2025-09-14 12:08:15 +0530

    Create Degree-of-polynomial.java

---
+++ Degree-of-polynomial.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner (System.in);
+		int t =sc.nextInt();
+		while(t-->0){
+		    int n = sc.nextInt();
+		    int arr[]=new int[n];
+		    for(int i=0;i<n;i++){
+		        arr[i] = sc.nextInt();
+		    }
+		    int degree=0;
+		    for(int i=n-1;i>=0;i--){
+		        if(arr[i]!=0){
+		            degree=i;
+		            break;
+		        }
+		    }
+		    System.out.println(degree);
+		}
+
+	}
+}
