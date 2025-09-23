addd97178ef1a8ac1bf9feaa72631e97fa55e30f
Praveena Venkatraj <praveena655.cse@gmail.com>
2025-09-23 18:03:32 +0530

    Create Vowel-or-constant.java

---
+++ Vowel-or-constant.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		char c = sc.next().charAt(0);
+		if(c=='A'||c=='E'||c=='I'||c=='O'||c=='U'){
+		    System.out.println("Vowel");
+		}
+		else{
+		    System.out.println("Consonant");
+		}
+
+	}
+}
