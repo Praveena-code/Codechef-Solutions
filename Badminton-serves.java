355b95d65fca58eb9bfece278cd1cf9469d9fbed
Praveena Venkatraj <praveena655.cse@gmail.com>
2025-10-01 19:24:58 +0530

    Create Badminton-serves.java

---
+++ Badminton-serves.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t= sc.nextInt();
+		while(t-->0){
+		    int num = sc.nextInt();
+		    int m = num/2;
+		    System.out.println(m+1);
+		}
+
+	}
+}
